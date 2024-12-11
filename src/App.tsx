69058c889302e9f24ed0f3e37daa34b1ccbe487e
Thurzas <mathieu.miot@gmail.com>
2024-12-11 14:18:45 +0100

    forgot to survey format code, so i linted

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,40 @@
 import { useState } from "react";
 
 import viteLogo from "/vite.svg";
-import reactLogo from "./assets/react.svg";
 import pokedex from "./assets/pokedexClosed.svg";
+
+import reactLogo from "./assets/react.svg";
 
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+	const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Projet : Pokédex (Wild Code School édition)</h1>
-      <div className="card">
-        <button type="button" onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-        <img className="pokedex" src={pokedex} />
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+	return (
+		<>
+			<div>
+				<a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+					<img src={viteLogo} className="logo" alt="Vite logo" />
+				</a>
+				<a href="https://react.dev" target="_blank" rel="noreferrer">
+					<img src={reactLogo} className="logo react" alt="React logo" />
+				</a>
+			</div>
+			<h1>Projet : Pokédex (Wild Code School édition)</h1>
+			<div className="card">
+				<button type="button" onClick={() => setCount((count) => count + 1)}>
+					count is {count}
+				</button>
+				<p>
+					Edit <code>src/App.tsx</code> and save to test HMR
+				</p>
+				<img className="pokedex" src={pokedex} alt="pokedex" />
+			</div>
+			<p className="read-the-docs">
+				Click on the Vite and React logos to learn more
+			</p>
+		</>
+	);
 }
 
 export default App;
