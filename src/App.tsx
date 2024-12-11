cb2b48ffb91ec74d78da9147732f00797f9d0872
Thurzas <mathieu.miot@gmail.com>
2024-12-11 15:09:50 +0100

    component 1 : PokemonCard

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,20 @@
 import { useState } from "react";
-
-import viteLogo from "/vite.svg";
-
-import pokedex from "./assets/pokedexClosed.svg";
-import reactLogo from "./assets/react.svg";
-
+import PokedexClosed from "./assets/pokedexClosed.svg";
+import PokedexOpen from "./assets/pokedexOpen.svg";
+import PokemonCard from "./components/PokemonCard";
 import "./App.css";
 
 function App() {
-	const [count, setCount] = useState(0);
 
 	return (
-		<>
-			<div>
-				<a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank" rel="noreferrer">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Projet : Pokédex (Wild Code School édition)</h1>
-			<div className="card">
-				<button type="button" onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-				<img className="pokedex" src={pokedex} alt="pokedex" />
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
+		<>        
+			<section>
+				<h1>Projet : Pokédex (Wild Code School édition)</h1>
+        <div className="pokemonCard">
+        <PokemonCard />
+        </div>
+				<img className="pokedex" src={PokedexOpen} alt="pokedex" />
+			</section>
 		</>
 	);
 }
---
+++ src/components/PokemonCard.tsx
@@ -0,0 +1,12 @@
+function PokemonCard(){
+    
+    return(
+    <>
+    <figure className="CardCaption">
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" alt="Bulbasaur" />
+        <figcaption>Bulbasaur</figcaption>
+    </figure>
+    </>
+    );
+}
+export default PokemonCard;
